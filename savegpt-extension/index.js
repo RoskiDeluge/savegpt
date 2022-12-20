e0a93bcb885352d40f521fcd7965b6453d599eee
RoskiDeluge <roberto.delgado@gmail.com>
2022-12-20 12:10:23 -1000

    First commit

--- savegpt-extension/index.js
+++ savegpt-extension/index.js
@@ -1,13 +1,13 @@
 let isRequesting = false;
 
 function init() {
-  const shareButton = createBtn();
+  const saveButton = createBtn();
 
   const buttonsWrapper = document.querySelector(
     "#__next main form > div div:nth-of-type(1)"
   );
 
-  buttonsWrapper.appendChild(shareButton);
+  buttonsWrapper.appendChild(saveButton);
 
   const textareaElement = document.querySelector("#__next main form textarea");
 
@@ -15,19 +15,19 @@ function init() {
 
   document.body.addEventListener("keydown", (event) => {
     if (event.keyCode === 13) {
-      showIfNotLoading(submitButton, shareButton);
+      showIfNotLoading(submitButton, saveButton);
     }
   });
 
   submitButton.addEventListener("click", (event) => {
-    showIfNotLoading(submitButton, shareButton);
+    showIfNotLoading(submitButton, saveButton);
   });
 
-  shareButton.addEventListener("click", async () => {
+  saveButton.addEventListener("click", async () => {
     if (isRequesting) return;
     isRequesting = true;
-    shareButton.textContent = "Sharing...";
-    shareButton.style.cursor = "initial";
+    saveButton.textContent = "Sharing...";
+    saveButton.style.cursor = "initial";
 
     const threadContainer = document.querySelector(
       "#__next main div:nth-of-type(1) div:nth-of-type(1) div:nth-of-type(1) div:nth-of-type(1)"
@@ -71,10 +71,10 @@ function init() {
     window.open(url, "_blank");
 
     setTimeout(() => {
-      shareButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3 h-3">
+      saveButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3 h-3">
       <path stroke-linecap="round" stroke-linejoin="round" d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z" />
     </svg>Share`;
-      shareButton.style.cursor = "pointer";
+      saveButton.style.cursor = "pointer";
       isRequesting = false;
     }, 1000);
   });
